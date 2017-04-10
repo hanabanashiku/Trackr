148090e59dc4a612e2ebe441804a648f3fca639f
Michael MacLean <elemaster96@aol.com>
2017-04-09 21:53:35 -0400

    Initial commit

---
+++ Trackr.core/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Trackr.core {
+    public class Program {
+        public static Settings UserSettings;
+
+        public static void Main(){
+            UserSettings = Settings.Load();
+        }
+    }
+}
---
+++ Trackr.core/Settings.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace Trackr.core {
+    [Serializable]
+    public class Settings {
+        public byte[] Entropy { get; private set; }
+
+        private Settings(){
+            Entropy = new byte[20];
+            using(var rng = new RNGCryptoServiceProvider())
+                rng.GetBytes(Entropy);
+        }
+
+        public static Settings Load(){
+            return null;
+        }
+
+        public void Save(){
+
+        }
+
+        ~Settings(){
+            Save();
+        }
+    }
+}

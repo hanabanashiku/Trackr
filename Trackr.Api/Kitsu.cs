f7b111a24b9cab7ac8b7326b412ac5f12b501a74
Michael MacLean <elemaster96@aol.com>
2017-05-02 12:21:09 -0400

    Kitsu AddAnime()

--- Trackr.Api/Kitsu.cs
+++ Trackr.Api/Kitsu.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Json;
 using System.Net;
+using System.Net.Http.Headers;
 using Trackr.Core;
 
 namespace Trackr.Api {
@@ -20,21 +22,20 @@ namespace Trackr.Api {
         private const string ClientId = "dd031b32d2f56c990b1425efe6c42ad847e7fe3ab46bf1299f05ecd856bdb7dd";
         private const string ClientSecret = "54d7307928f63414defd96399fc31ba847961ceaecef3a5fd93144e960c0e151";
         private const string UrlBase = "https://kitsu.io/api/17";
-        private const string Entries = "library-entries";
-        private const string Anime = "anime";
-        private const string Manga = "manga";
+        private const string Users = UrlBase + "/users";
 
         private readonly HttpClient _client;
         private UserPass _clientLogin;
-        private string _accessToken;
         private DateTime _expiration;
+        private int _userId;
 
         public Kitsu(UserPass credentials){
             _clientLogin = credentials;
             _client = new HttpClient();
             _client.DefaultRequestHeaders
-                .TryAddWithoutValidation("accept", ContentType);
-            _accessToken = null;
+                .TryAddWithoutValidation("Accept", ContentType);
+            _client.DefaultRequestHeaders
+                .TryAddWithoutValidation("Content-Type", ContentType);
             _expiration = DateTime.Now;
         }
 
@@ -50,7 +51,7 @@ namespace Trackr.Api {
             if(response.StatusCode != HttpStatusCode.OK)
                 return false;
             var json = JsonValue.Parse(response.Content.ReadAsStringAsync().Result);
-            _accessToken = json["access_token"];
+            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(json["token_type"], json["access_token"]);
             _expiration = DateTime.Now.AddSeconds(double.Parse(json["expires_in"]));
             return true;
         }
@@ -61,7 +62,81 @@ namespace Trackr.Api {
         /// <returns>True on success (200OK), false on failure (400, 401)</returns>
         /// <remarks>Authentication will be done implicitly  after the token expires.</remarks>
         public async Task<bool> VerifyCredentials(){
-            return await Authenticate();
+            if(await Authenticate() == false)
+                return false;
+
+            // TODO: Get _userId
+            var json = new JsonObject() {
+
+            };
+
+            return true;
+        }
+
+        public async Task<bool> AddAnime(int id, ApiEntry.ListStatuses status){
+            if(DateTime.Now >= _expiration) await Authenticate();
+
+            var data = new JsonObject() {
+                ["data"] = new JsonObject {
+                    ["type"] = "libraryEntries",
+                    ["attributes"] = new JsonObject() {
+                        ["status"] = ListStatusToString(status)
+                    },
+                    ["relationships"] = new JsonObject() {
+                        ["user"] = new JsonObject() {
+                            ["data"] = new JsonObject() {
+                                ["id"] = _userId,
+                                ["type"] = "users"
+                            }
+                        }
+                    },
+                    ["media"] = new JsonObject() {
+                        ["data"] = new JsonObject() {
+                            ["id"] = id,
+                            ["type"] = "anime"
+                        }
+                    }
+                }
+            };
+
+            var response = await _client.PostAsync(UrlBase, new StringContent(data));
+            return response.StatusCode == HttpStatusCode.OK;
+        }
+
+        private static string ListStatusToString(ApiEntry.ListStatuses status){
+            // ReSharper disable once SwitchStatementMissingSomeCases
+            switch(status) {
+                case ApiEntry.ListStatuses.Completed:
+                    return "completed";
+                case ApiEntry.ListStatuses.Current:
+                    return "current";
+                case ApiEntry.ListStatuses.Dropped:
+                    return "dropped";
+                case ApiEntry.ListStatuses.Planned:
+                    return "planned";
+                case ApiEntry.ListStatuses.OnHold:
+                    return "on_hold";
+                // NotInList - this should never be sent to the server
+                default:
+                    return string.Empty;
+            }
+        }
+
+        private static ApiEntry.ListStatuses StringToListStatus(string status){
+            switch(status) {
+                case "completed":
+                    return ApiEntry.ListStatuses.Completed;
+                case "current":
+                    return ApiEntry.ListStatuses.Current;
+                case "dropped":
+                    return ApiEntry.ListStatuses.Dropped;
+                case "planned":
+                    return ApiEntry.ListStatuses.Planned;
+                case "on_hold":
+                    return ApiEntry.ListStatuses.OnHold;
+                default: // should not be received from the server
+                    return ApiEntry.ListStatuses.NotInList;
+            }
         }
     }
 }

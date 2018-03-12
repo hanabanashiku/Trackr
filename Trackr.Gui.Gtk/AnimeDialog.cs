ffdb99dbdf05ed9ffb3079db595f03973066efdc
Michael MacLean <elemaster96@aol.com>
2018-03-12 16:35:48 -0400

    Begin Anime Dialog list elements

--- Trackr.Gui.Gtk/AnimeDialog.cs
+++ Trackr.Gui.Gtk/AnimeDialog.cs
@@ -21,14 +21,16 @@ namespace Trackr.Gui.Gtk {
 		/// <summary>
 		/// A copy of the given media with updated changes.
 		/// </summary>
-		public Anime Result;
+		public readonly Anime Result;
 		
-		private Anime _original;
+		private readonly Anime _original;
 		private bool _changed;
 
 		private Notebook _nb;
 		private VBox _infoBox, _editBox, _episodeBox, _characterBox;
-		private SpinButton _episodeSpin;
+		private SpinButton _episodeSpin, _scoreSpin;
+		private ComboBox _statusBox;
+		private TextView _notesEntry; // deal with dates
 		private Button _okButton, _cancelButton, _deleteButton;
 		
 		/// <summary>
@@ -64,20 +66,20 @@ namespace Trackr.Gui.Gtk {
 			var adj = new Adjustment(_original.CurrentEpisode, 0, max, 1, 10, 0);
 			_episodeSpin = new SpinButton(adj, 1, 0);
 			
-			_deleteButton = new Button(new Image(Stock.Delete, IconSize.Button));
+			_deleteButton = new Button(new Image(Stock.Remove, IconSize.Button));
 			_okButton = new Button("OK");
 			_cancelButton = new Button("Cancel");
 		}
 
 		private void Build() {
-			VBox.BorderWidth = 20;
+			
 			
 			// build the header
-			var header = new HBox(false, 0); // contains the top
+			var header = new HBox(false, 5); // contains the top
 			var imagebox = new VBox(); // contains the picture
 			try { // get the image
 				var stream = _original.GetCover().Result;
-				var cover = new Pixbuf(stream, 134, 190);
+				var cover = new Pixbuf(stream, 113, 159);
 				imagebox.PackStart(new Image(cover), false, false, 0);
 			}
 			catch(Exception e) {
@@ -99,17 +101,32 @@ namespace Trackr.Gui.Gtk {
 			spinbox.PackStart(new Label("Episode") { Justify = Justification.Right}, false, false, 20);
 			spinbox.Add(_episodeSpin);
 			_episodeSpin.SetSizeRequest(50, 22);
+			_episodeSpin.ValueChanged += OnEpisodeChanged;
 			spinbox.Add(new Label($"/ {_original.Episodes}"));
 			spinbox.PackEnd(new VBox(), true, true, 100); // push everything left
 			titlebox.Add(spinbox);
-			var databox = new VBox(true, 1); // contains basic series data
-			databox.PackStart( new Label($"Type: \t{Enum.GetName(typeof(Anime.ShowTypes), _original.Type)}"), true, true, 5);
-			databox.Add(new Label($"Status: \t{Enum.GetName(typeof(Anime.RunningStatuses), _original.Status)}"));
-			databox.Add(new Label($"Score: \t{_original.Score}"));
-			titlebox.PackStart(new Alignment(0, 1, 0, 0) {databox}, true, true, 10);
+			var labels = new[] {
+				$"Type: \t\t{Enum.GetName(typeof(Anime.ShowTypes), _original.Type)}",
+				$"Season: \t{_original.Season}",
+				$"Status: \t{Enum.GetName(typeof(Anime.RunningStatuses), _original.Status)}",
+				$"Score:\t\t{_original.Score}"
+			};
+			foreach (var s in labels) {
+				var hbox = new HBox(false, 10);
+				hbox.PackStart(new Label(s), false, false, 20);
+				hbox.PackEnd(new VBox(), true, true, 100);
+				titlebox.Add(hbox);
+			}
 			header.Add(titlebox);
 			VBox.PackStart(header, false, false, 5);
-			VBox.Add(new HBox() {_nb});
+			
+			// List Values
+			var listbox = new VBox();
+
+			_nb.AppendPage(listbox, new Label("List"));
+			
+			// Notebook
+			VBox.Add(_nb);
 			
 			// buttons
 			var buttonbox = new HBox(false, 5);
@@ -122,6 +139,12 @@ namespace Trackr.Gui.Gtk {
 			_okButton.SetSizeRequest(70, 30);
 			_cancelButton.SetSizeRequest(70, 30);
 			_deleteButton.Visible = _original.ListStatus != ApiEntry.ListStatuses.NotInList; // don't show the button if we cant delete
+		}
+
+		private void OnEpisodeChanged(object o, EventArgs args) {
+			var spin = (SpinButton) o;
+			Result.CurrentEpisode = (int)spin.Value;
+			_changed = true;
 		}
 	}
 }
